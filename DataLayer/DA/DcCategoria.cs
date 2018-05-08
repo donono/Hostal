2ca27af278e65b503df76a5a8df0fbb547291a8b
Daniel garcia <holapodaniel@hotmail.cl>
2018-05-08 14:59:47 -0300

    da wae

--- DataLayer/DA/DcCategoria.cs
+++ DataLayer/DA/DcCategoria.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
+using EntitiesLayer;
 
 namespace DataLayer.DA
 {
     public class DcCategoria
     {
+        public DataSet ReadAll()
+        {
+            string strConnection = ConfigurationManager.ConnectionStrings["ConnDaniel2"].ConnectionString;
+            OracleConnection oraCon = new OracleConnection(strConnection);
+            OracleCommand oraCmd = new OracleCommand();
+            oraCmd.Connection = oraCon;
+            DataSet ds = new DataSet();
+            try
+            {
+                oraCon.Open();
+                OracleDataAdapter DA = new OracleDataAdapter(@"SELECT * FROM CATEGORIA", oraCon);
+
+                DataSet DS = new DataSet();
+                DA.Fill(DS);
+                return DS;
+            }
+            catch (Exception ex)
+            { throw new Exception(ex.Message); }
+            finally
+            { if (oraCon.State == ConnectionState.Open) oraCon.Close(); oraCmd.Dispose(); }
+        }
+
     }
 }
---
+++ PresentationLayer/paginas/test.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLayer.DC;
+
+namespace PresentationLayer.paginas
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            llenarDDLCategorias();
+        }
+
+        public void llenarDDLCategorias()
+        {
+            try
+            {
+                dllCategorias.DataSource = new BcCategoria().ReadAll();
+                dllCategorias.DataTextField = "descripcion";   // "nombre del campo en la bd"
+                dllCategorias.DataValueField = "id_categoria";
+                dllCategorias.DataBind();
+                dllCategorias.Items.Insert(0, new ListItem(" Seleccione ", "0"));
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
